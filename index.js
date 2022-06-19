a270274071d6bde14f8e08e85468939bbe261534
waterbinnn <ewaterbin@naver.com>
2022-06-19 14:39:30 +0900

    비밀번호 암호화

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+//필요한 모듈
 const express = require("express");
 const app = express();
 const port = 5000;
-const bodyParser = require("body-parser");
-const config = require("./config/key");
 
+//몽고디비 연결 호스트 설정
+const config = require("./config/key");
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 //application/json
-app.use(bodyParser.json());
+app.use(express.json());
 
+//몽고디비 연결
 const mongoose = require("mongoose");
 mongoose
   .connect(config.mongoURI, {
@@ -19,18 +21,21 @@ mongoose
     // useCreateIndex: true,
     // useFindAndModify: false,
   })
+  //성공시
   .then(() => console.log("MongoDB Connected..."))
+  //실패시
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("잘하네?");
+  res.send("Good");
 });
 
 app.post("/register", (req, res) => {
   //회원가입할때 필요한 정보들을 client에서 가져오면
-  // 그것들을 데이터 베이스에 넣어준다
   const user = new User(req.body);
-
+  
+  // user 데이터 베이스에 넣어준다
+  //save 하기 전에 비밀번호를 암호화 해줘야한다
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -31,6 +33,32 @@ const userSchema = mongoose.Schema({
   },
 });
 
+//pre : mongoose 에서 가져온 메서드 , 저장하기 전에 뭔가를 하고 끝나면 next 를 진행
+userSchema.pre('save',function(next){
+  
+  //이걸 통해 password 가져올 수 있다
+  let user = this;
+
+  if (user.isModified('password')){
+
+    
+    //비밀번호를 암호화 시킨다. 
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if(err) return next(err)
+      
+      //hash 가 암호화된 비밀번호
+      bcrypt.hash(user.password, salt, function(err,hash){
+        if(err) return next(err)
+        user.password = hash //암호화하는데 성공하면 hash 로 만들어주는 것
+        next() //완성됐으면 다시 next 로 돌아가게 
+      } )
+    });
+  }
+  });
+
+
 const User = mongoose.model("User", userSchema);
+
+
 
 module.exports = { User };
